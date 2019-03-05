a29c101302fbb40e6ac80af0602e35de27a98870
Daniel Mahaffy <danielmahaffy@gmail.com>
2019-03-05 08:46:07 +0400

    Added support for self-hosted tinymce

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -16,6 +16,10 @@ class FieldServiceProvider extends ServiceProvider
     public function boot()
     {
         Nova::serving(function (ServingNova $event) {
+            $url = config('nova.tinymce_url',null);
+            if(!is_null($url)){
+                Nova::script("TinyMCE", config('nova.tinymce_url'));
+            }
             Nova::script('Nova-TinyMCE', __DIR__.'/../dist/js/field.js');
             Nova::style('Nova-TinyMCE', __DIR__.'/../dist/css/field.css');
             Nova::provideToScript([
