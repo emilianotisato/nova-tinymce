2038583865bd39a2f08a96f28a22e8733810875a
Yuriy Martini <y.martini@soluzionesoftware.com>
2019-11-28 10:14:56 +0100

    (Re)implemented self hosted ability

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -15,16 +15,14 @@ class FieldServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->publishes([
+            realpath(__DIR__ . '/../dist/tinymce') => public_path('vendor/tinymce'),
+        ], 'public');
+
         Nova::serving(function (ServingNova $event) {
-            $url = config('nova.tinymce_url',null);
-            if(!is_null($url)){
-                Nova::script("TinyMCE", config('nova.tinymce_url'));
-            }
+            Nova::script('Nova-TinyMCE-tinymce', __DIR__.'/../dist/js/tinymce.js');
             Nova::script('Nova-TinyMCE', __DIR__.'/../dist/js/field.js');
             Nova::style('Nova-TinyMCE', __DIR__.'/../dist/css/field.css');
-            Nova::provideToScript([
-                'tinymce_api_key' => config('nova.tinymce_api_key'),
-            ]);
         });
     }
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,7 @@ let mix = require('laravel-mix')
 
 mix.setPublicPath('dist')
    .js('resources/js/field.js', 'js')
-   .sass('resources/sass/field.scss', 'css')
+   .js('resources/js/tinymce.js', 'js')
+   .sass('resources/sass/field.scss', 'css');
+
+mix.copyDirectory('node_modules/tinymce/skins', 'dist/tinymce/skins');
